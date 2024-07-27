cbbe7b812e60d8f456937ed67e32f4fa5275d750
Chris Davison <c.jr.davison@gmail.com>
2024-07-27 14:16:18 +0100

    use skim for fuzzyfind and selection

--- src/util.rs
+++ src/util.rs
@@ -1,27 +1,33 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
+use skim::prelude::*;
+use std::io::Cursor;
 
-pub fn choose_from_list(ls: &[impl ToString], filter: Option<String>) -> Result<Vec<String>> {
-    let matching: Vec<_> = ls
+pub fn choose_from_list(ls: &[String], filter: Option<String>) -> Result<Vec<String>> {
+    let options = SkimOptionsBuilder::default().multi(true).build().unwrap();
+
+    let inputstr = if let Some(query) = filter {
+        ls
+            .iter()
+            .filter(|title| title.to_lowercase().contains(&query))
+            .map(|title| title.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    } else {
+        ls.join("\n")
+    };
+    // `SkimItemReader` is a helper to turn any `BufRead` into a stream of `SkimItem`
+    // `SkimItem` was implemented for `AsRef<str>` by default
+    let item_reader = SkimItemReader::default();
+    let items = item_reader.of_bufread(Cursor::new(inputstr));
+
+    // `run_with` would read and show items from the stream
+    let selected_books = Skim::run_with(&options, Some(items))
+        .map(|out| out.selected_items)
+        .unwrap_or_default();
+
+    let titles = selected_books
         .iter()
-        .filter(|key| match filter {
-            None => true,
-            Some(ref f) => key.to_string().to_lowercase().contains(f),
-        })
+        .map(|x| x.output().to_string())
         .collect();
-    for (i, key) in matching.iter().enumerate() {
-        println!("{}: {}", i, key.to_string());
-    }
-    let mut response = String::new();
-    std::io::stdin()
-        .read_line(&mut response)
-        .with_context(|| "Failed to get response for which books to export".to_string())?;
-    let mut books = Vec::new();
-    for choice in response.split(|c| " ,".contains(c)) {
-        let choice: usize = choice
-            .trim()
-            .parse()
-            .with_context(|| format!("Failed to parse book choice `{}`", choice))?;
-        books.push(matching[choice].to_string());
-    }
-    Ok(books)
+    Ok(titles)
 }
